22e0d3d4ed808c279cf8849b1dee1c587af9e4c4
Jaime Pillora <dev@jpillora.com>
2016-02-28 22:21:25 +1100

    Moved go version-specific into separate files, move common code out to archive.go

--- archive.go
+++ archive.go
@@ -1,10 +1,6 @@
-// +build go1.6
-
 package archive
 
 import (
-	"archive/zip"
-	"compress/flate"
 	"compress/gzip"
 	"errors"
 	"io"
@@ -64,23 +60,6 @@ func NewTarGzWriter(dst io.Writer) *Archive {
 
 func NewZipWriter(dst io.Writer) *Archive {
 	a, _ := NewWriter(".zip", dst)
-	return a
-}
-
-// NewCompressedZipWriter returns archive compressed with DEFLATE data format
-func NewCompressedZipWriter(dst io.Writer, level int) *Archive {
-	a := &Archive{
-		DirMaxSize:  -1,
-		DirMaxFiles: -1,
-		dst:         dst,
-	}
-
-	czip := newCompressedZipArchive(dst)
-	czip.writer.RegisterCompressor(zip.Deflate, func(out io.Writer) (io.WriteCloser, error) {
-		return flate.NewWriter(out, level)
-	})
-
-	a.archive = czip
 	return a
 }
 
